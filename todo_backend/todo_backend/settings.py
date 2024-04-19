34ce00ddff019d354b07e210253298d4a5bd2957
keshav119 <channakeshav99.ck@gmail.com>
2024-04-19 17:13:23 +0530

    s

--- todo_backend/todo_backend/settings.py
+++ todo_backend/todo_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'Gr0wLyxlmIeq0clXzVl1W3Mwx2h1KzWNr/ZYtH+3e13G1dKPT9kdUXNWxeXcXB0bai
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
